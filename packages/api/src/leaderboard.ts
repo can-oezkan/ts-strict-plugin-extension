903aa2e43adbdccdcd92bf1f25d91d679acce1c9
Can Özkan <can@oezkan.me>
2024-12-20 04:15:34 +0100

    feat: setup api

---
+++ packages/api/src/leaderboard.ts
@@ -0,0 +1,5 @@
+import { TSStrictLeaderboard, TSStrictLeaderboardConfig } from '@ts-strict-plugin-extension/common';
+
+export function getLeaderboard(config: TSStrictLeaderboardConfig): TSStrictLeaderboard {
+  return null!;
+}
