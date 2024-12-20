d94044889d911b5e48f395d24ed0c86626c9657d
Can Özkan <can@oezkan.me>
2024-12-20 04:12:36 +0100

    feat: add interface for leaderboard

---
+++ packages/common/index.ts
@@ -0,0 +1,2 @@
+export * from './src/ts-strict-leaderboard-config.js';
+export * from './src/ts-strict-leaderboard.js';
---
+++ packages/common/src/ts-strict-leaderboard.ts
@@ -0,0 +1,4 @@
+export type TSStrictLeaderboard = {
+  name: string;
+  score: number;
+}[];
