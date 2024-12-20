d51a0814be567afccd3b5864b6c2b1958fef62e6
Can Özkan <can@oezkan.me>
2024-12-20 17:25:58 +0100

    docs: add docs to test-utils

--- packages/test-utils/src/setup.ts
+++ packages/test-utils/src/setup.ts
@@ -2,6 +2,19 @@ import { execSync } from 'node:child_process';
 import fs from 'node:fs';
 import path from 'node:path';
 
+/**
+ * Initializes a git repository in the given directory and applies the initial patch `0001-init.patch`.
+ * The initial patch configures a typescript workspace with 5 ts files that are not strict:
+ * - sum.ts
+ * - string_suffix.ts
+ * - string_prefix.ts
+ * - point.ts
+ * - multiply.ts
+ *
+ * The commit author is set to `Contributor 0` and the email is set to `contributor0@example.com`.
+ *
+ * @param directory The directory to initialize the git repository in.
+ */
 export function init(directory: string) {
   fs.rmdirSync(directory);
   fs.mkdirSync(directory);
@@ -13,6 +26,13 @@ export function init(directory: string) {
   execSync(`git am ${patchFile}`, { cwd: directory });
 }
 
+/**
+ * Adds the strict plugin to the given directory and applies the patch `0002-init-ts-strict-plugin.patch`.
+ *
+ * The commit author is set to `Contributor 0` and the email is set to `contributor0@example.com`.
+ *
+ * @param directory The directory to add the strict plugin to.
+ */
 export function setupStrictPlugin(directory: string) {
   const patchFile = path.join(
     import.meta.dirname,
@@ -24,6 +44,12 @@ export function setupStrictPlugin(directory: string) {
   execSync(`git am ${patchFile}`, { cwd: directory });
 }
 
+/**
+ * Applies the patch `0003-strict-sum.ts.patch` to the given directory that makes the file `sum.ts` strict.
+ * The commit author is set to `Contributor 1` and the email is set to `contributor1@example.com`
+ *
+ * @param directory The directory to apply the patch to.
+ */
 export function strictSum(directory: string) {
   const patchFile = path.join(
     import.meta.dirname,
@@ -35,6 +61,13 @@ export function strictSum(directory: string) {
   execSync(`git am ${patchFile}`, { cwd: directory });
 }
 
+/**
+ * Applies the patch `0004-strict-string_suffix.ts.patch` to the given directory that makes the file `string_suffix.ts` strict.
+ *
+ * The commit author is set to `Contributor 1` and the email is set to `contributor1@example.com`.
+ *
+ * @param directory The directory to apply the patch to.
+ */
 export function strictStringSuffix(directory: string) {
   const patchFile = path.join(
     import.meta.dirname,
@@ -46,6 +79,13 @@ export function strictStringSuffix(directory: string) {
   execSync(`git am ${patchFile}`, { cwd: directory });
 }
 
+/**
+ * Applies the patch `0005-strict-string_prefix.ts.patch` to the given directory that makes the file `string_prefix.ts` strict.
+ *
+ * The commit author is set to `Contributor 2` and the email is set to `contributor2@example.com`.
+ *
+ * @param directory The directory to apply the patch to.
+ */
 export function strictStringPrefix(directory: string) {
   const patchFile = path.join(
     import.meta.dirname,
@@ -57,6 +97,13 @@ export function strictStringPrefix(directory: string) {
   execSync(`git am ${patchFile}`, { cwd: directory });
 }
 
+/**
+ * Applies the patch `0006-strict-point.ts.patch` to the given directory that makes the file `point.ts` strict.
+ *
+ * The commit author is set to `Contributor 3` and the email is set to `contributor3@example.com`.
+ *
+ * @param directory The directory to apply the patch to.
+ */
 export function strictPoint(directory: string) {
   const patchFile = path.join(
     import.meta.dirname,
@@ -68,6 +115,13 @@ export function strictPoint(directory: string) {
   execSync(`git am ${patchFile}`, { cwd: directory });
 }
 
+/**
+ * Applies the patch `0007-strict-multiply.ts.patch` to the given directory that makes the file `multiply.ts` strict.
+ *
+ * The commit author is set to `Contributor 4` and the email is set to `contributor4@example.com`.
+ *
+ * @param directory The directory to apply the patch to.
+ */
 export function strictMultiply(directory: string) {
   const patchFile = path.join(
     import.meta.dirname,
